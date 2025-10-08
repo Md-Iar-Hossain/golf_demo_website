5b0d3a78de7cfeb2980a2d7ecbda68ecc2649b58
Md-Iar-Hossain <mdiarhossain10@gmail.com>
2025-10-08 19:17:02 +0600

    making navbar responsive for all devices

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,35 @@
+const nav = document.querySelector('nav')
 const modal = document.querySelector('dialog')
 const modal_btn = document.querySelector('.search_icon')
 const close_btn = document.querySelector('.close_btn')
+const menu_bar = document.querySelector('.menu_bar')
 
 modal_btn.addEventListener('click', () => {
     modal.showModal()
-    document.body.style.overflow = 'hidden';
+    removeOverFlow(true)
 })
 close_btn.addEventListener('click', () => {
     modal.close()
-    document.body.style.overflow = 'auto';
+    removeOverFlow(false)
 })
+menu_bar.addEventListener('click', function() {
+    if(!nav.classList.contains('active')) {
+        nav.classList.add('active');
+        this.querySelector('img').src = 'img/close.svg';
+        this.style.animation = 'rotateIn 0.5s ease-in-out'
+        removeOverFlow(true)
+    } else {
+        nav.classList.remove('active');
+        this.querySelector('img').src = 'img/menu.svg'
+        this.style.animation = 'rotateOut 0.5s ease-in-out'
+        removeOverFlow(false)
+    }   
+})
+function removeOverFlow(boolean) {
+    if (boolean) {
+        document.body.style.overflow = 'hidden';
+    }
+    else {
+        document.body.style.overflow = 'auto';
+    }
+}
